6911e97b38f764227bb607627d4bd9029df404b7
Maicon Oliveira <mikon182@gmail.com>
2019-05-04 13:19:56 -0300

    start

---
+++ src/main/kotlin/br/com/booking/travel/ChipestTravelApplication.kt
@@ -0,0 +1,11 @@
+package br.com.booking.travel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChipestTravelApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChipestTravelApplication>(*args)
+}
